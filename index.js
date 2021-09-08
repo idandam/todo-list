faa7e16e6347a512e03b227c89ebd74f155dbb5f
idan-grimberg-damri <idan.grimberg.damri@gmail.com>
2021-09-08 17:16:45 +0300

    cleaned indexjs

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import TodoController from "./todo-controller.js"
 import TodoModel from "./todo-model.js"
-import TodoSorage from "./todo-storage.js";
-
-//localStorage.clear();
 
 new TodoController(new TodoModel());
 
